010d269453fdbe592b3842109ede1e093e93d3b8
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-11-03 03:34:41 +0700

    [EDIT] Import Font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,17 @@
 import { Fragment } from 'react'
 import NavBar from '../components/navbar'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600;700;800&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Athiti:wght@200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </Fragment>
